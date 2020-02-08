be60fc4561b6710f3c4b9bbb517469dafbeba17e
Nicolas VINCENT <nico.vince@gmail.com>
2020-02-08 19:20:16 +0100

    Adds moonlight sonata and remove debussy clair de lune

--- mb_external/board/raspberrypi/post-build_dl_music.sh
+++ mb_external/board/raspberrypi/post-build_dl_music.sh
@@ -5,15 +5,26 @@ mkdir -p "${MUSIC_DIR}"
 cd "${MUSIC_DIR}"
 
 # Debussy - Clair de lune
-FILENAME="Debussy - Clair de Lune"
+#FILENAME="Debussy - Clair de Lune"
+#FILE="${FILENAME}.mp3"
+#if [ ! -f "${FILE}" ]; then
+#    URL="https://www.youtube.com/watch?v=Yaxu4qEXDJs"
+#    youtube-dl -x --audio-format mp3 "${URL}" --exec "mv {} \"${FILE}\""
+#    ffmpeg -i "${FILE}" -ss 00:00:00.00 -t 00:05:10.00 -c copy "tmp.mp3"
+#    rm "${FILE}"
+#    mv tmp.mp3 "${FILE}"
+#fi
+
+FILENAME="Moonlight Sonata"
 FILE="${FILENAME}.mp3"
 if [ ! -f "${FILE}" ]; then
-    URL="https://www.youtube.com/watch?v=Yaxu4qEXDJs"
+    URL="https://www.youtube.com/watch?v=sbTVZMJ9Z2I"
     youtube-dl -x --audio-format mp3 "${URL}" --exec "mv {} \"${FILE}\""
-    ffmpeg -i "${FILE}" -ss 00:00:00.00 -t 00:05:10.00 -c copy "tmp.mp3"
+    ffmpeg -i "${FILE}" -ss 00:00:04.00 -t 00:07:00.00 -c copy "tmp.mp3"
     rm "${FILE}"
     mv tmp.mp3 "${FILE}"
 fi
+
 
 FILENAME="Une Libellule"
 FILE="${FILENAME}.mp3"
