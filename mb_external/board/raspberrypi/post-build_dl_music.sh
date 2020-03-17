6f808d0aa32196fc0822f8bbad17125db1bca513
Nicolas VINCENT <nico.vince@gmail.com>
2020-03-17 20:43:20 +0100

    Create script for each music which sets volume appropriately

--- mb_external/board/raspberrypi/post-build_dl_music.sh
+++ mb_external/board/raspberrypi/post-build_dl_music.sh
@@ -4,6 +4,15 @@ MUSIC_DIR=${TARGET_DIR}/media/music
 mkdir -p "${MUSIC_DIR}"
 cd "${MUSIC_DIR}"
 
+create_script()
+{
+    echo "#!/bin/sh" > "$3"
+    echo "sleep 0.5s" >> "$3"
+    echo "amixer -c 1 set PCM ${2}%" >> "$3"
+    echo "mpg123 \"/media/music/$1\"" >> "$3"
+    chmod +x "$3"
+}
+
 # Debussy - Clair de lune
 #FILENAME="Debussy - Clair de Lune"
 #FILE="${FILENAME}.mp3"
@@ -17,32 +26,37 @@ cd "${MUSIC_DIR}"
 
 FILENAME="Moonlight Sonata"
 FILE="${FILENAME}.mp3"
+SCRIPT="${FILENAME}.sh"
 if [ ! -f "${FILE}" ]; then
     URL="https://www.youtube.com/watch?v=sbTVZMJ9Z2I"
     youtube-dl -x --audio-format mp3 "${URL}" --exec "mv {} \"${FILE}\""
     ffmpeg -i "${FILE}" -ss 00:00:04.00 -t 00:07:00.00 -c copy "tmp.mp3"
     rm "${FILE}"
     mv tmp.mp3 "${FILE}"
+    create_script "${FILE}" 20 "${SCRIPT}"
 fi
 
 
 FILENAME="Une Libellule"
 FILE="${FILENAME}.mp3"
+SCRIPT="${FILENAME}.sh"
 if [ ! -f "${FILE}" ]; then
     URL="https://www.youtube.com/watch?v=3tJR-JIMaMo"
     youtube-dl -x --audio-format mp3 "${URL}" --exec "mv {} \"${FILE}\""
     ffmpeg -i "${FILE}" -ss 00:00:00.00 -t 00:02:15.00 -c copy "tmp.mp3"
     rm "${FILE}"
     mv tmp.mp3 "${FILE}"
+    create_script "${FILE}" 17 "${SCRIPT}"
 fi
 
 # Berceuse de Brahms
 FILENAME="Brahms - Berceuse"
 FILE="${FILENAME}.mp3"
-
+SCRIPT="${FILENAME}.sh"
 if [ ! -f "${FILE}" ]; then
     URL="https://www.youtube.com/watch?v=PDJqRR1X1QU"
     youtube-dl -x --audio-format mp3 "${URL}" --exec "mv {} \"${FILE}\""
+    create_script "${FILE}" 20 "${SCRIPT}"
 fi
 
 cd ${MUSIC_DIR}
